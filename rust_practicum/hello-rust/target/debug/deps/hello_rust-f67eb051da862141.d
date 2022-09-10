2e111bbed14a9a4b3fc157102849f846bdec1b8e
Oğuzhan Akçalı <oguzkhanakcali@gmail.com>
2022-09-10 18:55:28 +0300

    rust_hello

---
+++ rust_practicum/hello-rust/target/debug/deps/hello_rust-f67eb051da862141.d
@@ -0,0 +1,5 @@
+/Users/oguzhanakcali/D/proje/patika/rust_practicum/hello-rust/target/debug/deps/hello_rust-f67eb051da862141: src/main.rs
+
+/Users/oguzhanakcali/D/proje/patika/rust_practicum/hello-rust/target/debug/deps/hello_rust-f67eb051da862141.d: src/main.rs
+
+src/main.rs:
---
+++ rust_practicum/hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/Users/oguzhanakcali/D/proje/patika/rust_practicum/hello-rust/target/debug/hello-rust: /Users/oguzhanakcali/D/proje/patika/rust_practicum/hello-rust/src/main.rs
